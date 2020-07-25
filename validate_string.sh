c183a87f91e232e583329fa82f514bb4a881c9d2
sumeetisp <sumeetisp@gmail.com>
2020-07-25 16:32:01 +0530

    Update validate_string.sh

--- validate_string.sh
+++ validate_string.sh
@@ -3,14 +3,16 @@ source ./email.properties
 
 validateEmailAddr(){
         if [[ $1 =~ $regex ]] ; then
-          echo "$1 is valid email address."
+          echo "$2) $1 is valid email address."
         else
-          echo "$1 is not is not a valid email address."
+          echo "$2) $1" >> $output_file
         fi
 }
 
-
+n=0
+[ -e $output_file ] && rm $output_file
 while read line; do
+n=$((n+1))
 # reading each line
-validateEmailAddr $line
+validateEmailAddr "$line" $n
 done < $filename
