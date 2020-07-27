61912b4e3fea91cb0142eb32f8c0e2d48260739c
sumeetisp <sumeetisp@gmail.com>
2020-07-27 15:29:06 +0530

    Update validate_string.sh

--- validate_string.sh
+++ validate_string.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+#The regex matches email address that starts with Charachter or Number followed by any number of characters, numbers and some special characters. 
+#This can be followed by zero or more "." and again followed by any number of characters, numbers and special characters.
+#Regex will check for @ followed by any chars and numbers. These can then be followed by "." and again any number of chars and numbers.
+
+
 source ./email.properties
 
 validateEmailAddr(){
